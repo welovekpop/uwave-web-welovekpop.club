4bc603aa8135011cf717ea7645b6eb18bb80d869
Renée Kooi <renee@kooi.me>
2018-05-29 14:10:42 +0200

    [WLK-INSTANCE] Remove IP addresses from error reports

--- src/_wlk/bugsnag.js
+++ src/_wlk/bugsnag.js
@@ -2,9 +2,21 @@
 import bugsnag from 'bugsnag-js';
 import { version } from '../../package.json';
 
+let userId = null;
+try {
+  userId = localStorage.errorReportId;
+  if (!userId) {
+    userId = Math.random().toString(32).slice(2, 8);
+    localStorage.errorReportId = userId;
+  }
+} catch {
+  userId = 'anonymous';
+}
+
 const client = bugsnag({
   apiKey: 'a3246545081c8decaf0185c7a7f8d402',
   appVersion: version,
+  collectUserIp: false,
   /**
    * Add current user information.
    */
@@ -16,6 +28,11 @@ const client = bugsnag({
       report.user = {
         id: user._id,
         name: user.username,
+      };
+    } else {
+      report.user = {
+        id: userId,
+        name: 'Guest',
       };
     }
   },
