053325e6c777e5d83cb84e19466e8d0db61e54da
Renée Kooi <renee@kooi.me>
2018-06-09 21:14:23 +0200

    [WLK-INSTANCE] Anonymise bugsnag reports.

--- es/_wlk/bugsnag.js
+++ es/_wlk/bugsnag.js
@@ -1,24 +1,31 @@
 /* global window, uw */
 import bugsnag from 'bugsnag-js';
 import { version } from '../../package.json';
+var userId = null;
+
+try {
+  userId = localStorage.errorReportId;
+
+  if (!userId) {
+    userId = Math.random().toString(32).slice(2, 8);
+    localStorage.errorReportId = userId;
+  }
+} catch (_unused) {
+  userId = 'anonymous';
+}
+
 var client = bugsnag({
   apiKey: 'a3246545081c8decaf0185c7a7f8d402',
   appVersion: version,
+  collectUserIp: false,
 
   /**
    * Add current user information.
    */
   beforeSend: function beforeSend(report) {
-    var state = uw.store.getState();
-    var user = state.auth && state.auth.user;
-
-    if (user) {
-      // eslint-disable-next-line no-param-reassign
-      report.user = {
-        id: user._id,
-        name: user.username
-      };
-    }
+    report.user = {
+      id: userId
+    };
   }
 });
 window.bugsnag = client;
--- src/_wlk/bugsnag.js
+++ src/_wlk/bugsnag.js
@@ -17,24 +17,14 @@ const client = bugsnag({
   apiKey: 'a3246545081c8decaf0185c7a7f8d402',
   appVersion: version,
   collectUserIp: false,
+
   /**
    * Add current user information.
    */
   beforeSend(report) {
-    const state = uw.store.getState();
-    const user = state.auth && state.auth.user;
-    if (user) {
-      // eslint-disable-next-line no-param-reassign
-      report.user = {
-        id: user._id,
-        name: user.username,
-      };
-    } else {
-      report.user = {
-        id: userId,
-        name: 'Guest',
-      };
-    }
+    report.user = {
+      id: userId,
+    };
   },
 });
 
