3682d84ca7421d879f9d4af365202a2a5d25e15d
Renée Kooi <renee@kooi.me>
2018-06-12 13:11:46 +0200

    [WLK-INSTANCE] Add update script.

---
+++ update.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+PREV="$1"
+NEXT=wlk-instance
+
+echo old:
+git show -s --oneline $PREV | cat
+echo new:
+git show -s --oneline $NEXT | cat
+echo press ENTER to continue
+read x
+
+git checkout $PREV
+rm -r public
+npm ci
+npm run prod
+mv public public-old
+git checkout -- .
+
+git checkout $NEXT
+rm -r public
+npm ci
+npm run prod
+mv public public-new
+git checkout -- .
+
+rm -r public
+mkdir public
+cp public-old/* public
+cp public-new/* public
+
+# rm -r public-old public-new
